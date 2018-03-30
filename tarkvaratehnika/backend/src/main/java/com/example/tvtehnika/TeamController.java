073dbe22f434030fc7459e1d0ba311f6b37c672c
denissharlamov <36692322+denissharlamov@users.noreply.github.com>
2018-03-30 16:12:25 +0300

    Add files via upload

---
+++ tarkvaratehnika/backend/src/main/java/com/example/tvtehnika/TeamController.java
@@ -0,0 +1,55 @@
+package com.example.tvtehnika;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping(path="/api")
+public class TeamController {
+    @Autowired
+    TeamRepository teamRepository;
+
+    //get all teams
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping(path="/teams")
+    public @ResponseBody List<Team> getAllTeams() {
+        return teamRepository.findAll();
+    }
+
+    // create a new tournament
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping(path="/addTeam")
+    public @ResponseBody String createTeam(@RequestParam(value="name", required=true) String name,
+                                           @RequestParam(value="country", required=true) String country,
+                                           @RequestParam(value="contactPerson", required=true) String contactPerson,
+                                           @RequestParam(value="contactPhoneNumber", required=false) String contactPhoneNumber,
+                                           @RequestParam(value="contactEmail", required=true) String contactEmail,
+                                           @RequestParam(value="comment", required=false) String comment){
+
+
+        Team team = new Team();
+        team.setName(name);
+        team.setCountry(country);
+        team.setContactPerson(contactPerson);
+        team.setContactEmail(contactEmail);
+
+        if(contactPhoneNumber != null && !contactPhoneNumber.isEmpty()) { team.setContactPhoneNumber(contactPhoneNumber); }
+        if(comment != null && !comment.isEmpty()) { team.setComment(comment); }
+
+        teamRepository.save(team);
+        return "Saved";
+    }
+
+    // Get a Single Team
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping(path="/team/{id}")
+    public @ResponseBody Team getTeamById(@PathVariable(value = "id") Long teamId) {
+        return teamRepository.findById(teamId)
+                .orElseThrow(() -> new ResourceNotFoundException("Team not found"));
+    }
+
+}
---
+++ tarkvaratehnika/backend/src/main/java/com/example/tvtehnika/TeamRepository.java
@@ -0,0 +1,9 @@
+package com.example.tvtehnika;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
+
+}
