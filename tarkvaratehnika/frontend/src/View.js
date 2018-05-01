ac64ec6ee6b923eea3bbfd6e5884d6eb95a63702
ErgoA <ealtin7@gmail.com>
2018-05-01 20:50:44 +0300

    Add files via upload
    change match scores

--- tarkvaratehnika/frontend/src/View.js
+++ tarkvaratehnika/frontend/src/View.js
@@ -3,19 +3,17 @@ import React, { Component } from "react";
 const teamAPI = 'http://localhost:8080/api/teams/'
 const playerAPI = 'http://localhost:8080/api/playersByTeam/'
 const matchAPI = 'http://localhost:8080/api/match/'
+var team1Goals = 0;
+var team2Goals = 0;
+var scorers = [];
 
 class View extends Component {
 
   constructor() {
     super();
+    this.reply_click = this.reply_click.bind(this);
     this.state = {
-      data: [],
       playersData: [],
-      update: [],
-      team1: [],
-      team2: [],
-      matchData: [],
-      testTeam: [],
     };
   }
 
@@ -25,41 +23,64 @@ class View extends Component {
       .then((matchResponse) => matchResponse.json())
       .then((matchfindresponse) => {
         console.log(matchfindresponse.team1.name);
-        console.log(this.props.matchId + ' ' + newProps.matchId);
+        console.log(matchfindresponse);
         this.setState({
           testTeam:matchfindresponse.team1.name,
         })
+        localStorage.setItem('team1', matchfindresponse.team1.name);
+        localStorage.setItem('team1id', matchfindresponse.team1.id);
+        localStorage.setItem('team2', matchfindresponse.team2.name);
+        localStorage.setItem('team2id', matchfindresponse.team2.id);
+        localStorage.setItem('matchId', matchfindresponse.id);
+        localStorage.setItem('score', matchfindresponse.result);
       })
     }
   }
 
-  componentDidMount() {
-  const promises = [
-    fetch(teamAPI).then(resp => resp.json()),
-    fetch(playerAPI + 22).then(resp => resp.json())
-  ];
-  Promise.all(promises).then(([teamData, playerData]) => {
-    console.log(teamData);
-    console.log(playerData);
-    this.setState({
-      team1:teamData[0].name,
-      team2:teamData[1].name,
-      playersData: playerData,
-    })
-  });
-}
+  reply_click() {
+    var e = document.getElementById('teamSelection');
+    var chosen = e.value;
+    console.log(chosen);
 
-  reply_click = id => {
-    return () => {
-        this.setState({ targetId: id })
-    }
+    fetch(playerAPI + chosen)
+    .then((Response) => Response.json())
+    .then((findresponse) => {
+      this.setState({
+        playersData:findresponse,
+      })
+    })
   }
 
   updateScore() {
-    fetch('http://localhost:8080/api/updateMatch?matchId=15&result=2:2')
+    var team1 = localStorage.getItem('team1id');
+    var team2 = localStorage.getItem('team2id');
+    var playerName = document.getElementById('playerSelection').value;
+    var teamName = document.getElementById('teamSelection').value;
+    var matchId = localStorage.getItem('matchId');
+    console.log(teamName);
+    if (teamName == team1) {
+      team1Goals += 1;
+    } else if (teamName == team2) {
+      team2Goals += 1;
+    }
+    var score = team1Goals + ':' + team2Goals;
+    scorers.push(playerName);
+    console.log(team1 + ' ' + team2 + ' ' + teamName + ' ' + score + ' ' + scorers);
+    document.getElementById("scorersTest").innerHTML = scorers[0];
+    document.getElementById("liveScore").innerHTML = score;
+    fetch('http://localhost:8080/api/updateMatch?matchId=' + matchId + '&result=' + score);
   }
 
   render() {
+    var matchTeam1 = localStorage.getItem('team1');
+    var matchTeam1Id = localStorage.getItem('team1id');
+    var matchTeam2 = localStorage.getItem('team2');
+    var matchTeam2Id = localStorage.getItem('team2id');
+    var players = localStorage.getItem('players');
+    var matchScore = localStorage.getItem('score');
+    var matchId = localStorage.getItem('matchId');
+
+    console.log(matchTeam1 + ' ' + matchTeam2 + ' ' + matchTeam1Id + matchScore + matchId);
     return (
       <div class="container">
         <div class="row">
@@ -68,17 +89,17 @@ class View extends Component {
               <form>
                 <div class="form-group">
                   <label for="teamSelect">Select team:</label>
-                    <select class="form-control" id='?' onChange={this.reply_click}>
-                      <option>{this.state.testTeam}</option>
-                      <option>{this.state.team2}</option>
+                    <select class="form-control" id='teamSelection' onClick={this.reply_click}>
+                      <option value={matchTeam1Id}>{matchTeam1}</option>
+                      <option value={matchTeam2Id}>{matchTeam2}</option>
                     </select>
                 </div>
                 <div class="form-group">
                   <label for="playerSelect">Select player:</label>
-                  <select class="form-control" id="playerSelect">
+                  <select class="form-control" id="playerSelection">
                   {
                     this.state.playersData.map((dynamicData, key) =>
-                      <option>{dynamicData.name}</option>
+                      <option value={dynamicData.name}>{dynamicData.name}</option>
                   )}
                   </select>
                 </div>
@@ -89,9 +110,8 @@ class View extends Component {
               </form>
             <br></br>
             Match details<br></br>
-            Score: 1:0
-            {this.state.matchData}
-            <br></br>3'' Player A <i class="far fa-futbol"></i>
+            Score: <span id="liveScore"></span>
+            <br></br><span id="scorersTest"></span><i class="far fa-futbol"></i>
           </div>
         </div>
       </div>
