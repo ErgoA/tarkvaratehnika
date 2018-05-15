02c8c5580b3f02a63bc062d745cf5848d701a055
denissharlamov <36692322+denissharlamov@users.noreply.github.com>
2018-05-15 18:36:07 +0300

    Add files via upload

--- tarkvaratehnika/backend/src/main/java/com/example/tvtehnika/repository/PlayerRepository.java
+++ tarkvaratehnika/backend/src/main/java/com/example/tvtehnika/repository/PlayerRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.query.Param;
 import com.example.tvtehnika.model.*;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 @Repository
@@ -14,5 +15,14 @@ public interface PlayerRepository extends JpaRepository<Player, Integer> {
     @Query(value="SELECT * FROM player p where p.team_id = :team_id", nativeQuery=true)
     List<Player> findByTeamid(int team_id);
 	public Player findByName(String name);
+
+
+
+	public List<Player> findAllByOrderByGoalsDesc();
+
+
+    @Query(value = "SELECT name,goals,assists,yellow_cards,red_cards FROM player ORDER BY goals DESC", nativeQuery = true)
+    public List<Object[]> findstats();
+
 }
 
